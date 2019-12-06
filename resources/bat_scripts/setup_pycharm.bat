4667a6cafae8cfdd6b804d1b64a3e8480e7e06d4
tmikota <tmikota@gmail.com>
2019-12-06 09:13:40 -0500

    testing launch script with cgl

--- resources/bat_scripts/setup_pycharm.bat
+++ resources/bat_scripts/setup_pycharm.bat
@@ -3,8 +3,8 @@ if not exist "%HOMEPATH%\PycharmProjects\developer_onboarding" git clone https:/
 cd %HOMEPATH%\PycharmProjects\developer_onboarding
 pip install -r requirements.txt
 python %HOMEPATH%\PycharmProjects\developer_onboarding\src\bin\pycharm_setup.py -s=True -l=False
-git clone https://github.com/basepipe/cglumberjack.git %HOMEPATH%\PycharmProjects\cglumberjack
+if not exist "%HOMEPATH%\PycharmProjects\cglumberjack" git clone https://github.com/basepipe/cglumberjack.git %HOMEPATH%\PycharmProjects\cglumberjack
 cd %HOMEPATH%\PycharmProjects\cglumberjack
-pip install -r requirements
+pip install -r requirements.txt
 python %HOMEPATH%\PycharmProjects\developer_onboarding\src\bin\pycharm_setup.py -s=False -l=cgl
 PAUSE
